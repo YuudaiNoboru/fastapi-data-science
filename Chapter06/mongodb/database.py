b8b320b415b47ea85880e1025a1053176dbf980a
Lucas <vasconcelos.contabeis@gmail.com>
2024-11-18 16:52:28 -0300

    Resolução dos exercícios do Capitulo 10

--- Chapter06/mongodb/database.py
+++ Chapter06/mongodb/database.py
@@ -1,8 +1,10 @@
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 
-motor_client:AsyncIOMotorClient = AsyncIOMotorClient("localhost", 27017)
-
+# Connection to the whole server
+motor_client = AsyncIOMotorClient("mongodb://localhost:27017")
+# Single database instance
 database = motor_client["chapter06_mongo"]
 
-def get_databese() -> AsyncIOMotorDatabase:
+
+def get_database() -> AsyncIOMotorDatabase:
     return database
